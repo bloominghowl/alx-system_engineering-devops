3bda2e594e3ab0e450574f95b98bb0248607a089
Ayivi Angela <104152275+bloominghowl@users.noreply.github.com>
2023-06-26 22:40:15 +0000

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,40 +1,22 @@
-nstall & configure nginx on a server using Puppet
+#!/usr/bin/env bash
+# Using Puppet| Install Nginx server, setup and configuration
 
-$config = "server {
-	listen 80 default_server;
-        listen [::]:80 default_server;
-
-        root /var/www/html;
-        index index.html;
-
-        location /redirect_me {
-                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-        }
-}"
-
-package { 'nginx':  # Installs an Nginx server
-ensure	=> 'installed',
+package { 'nginx':
+  ensure => 'installed'
 }
 
-file { 'index.html':
-ensure	=> 'present',
-path	=> '/var/www/html/index.html',
-content	=> 'Hello World!',
-mode	=> '0644'
+file { '/var/www/html/index.html':
+  content => 'Hello World',
 }
 
-file { 'server_config':
-ensure	=> 'present',
-path 	=> '/etc/nginx/sites-available/default',
-content => $config
+file_line { 'redirection-301':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-exec { 'service nginx restart':
-path	=> ['/usr/sbin', '/usr/bin']
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
-
-# OR
-# exec {'install':
-#   provider => shell,
-#   command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/Dikachis permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
-# }
